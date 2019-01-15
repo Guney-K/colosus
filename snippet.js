3e60f81d3d8a70bfc982b823df50695de2476614
Guney-K <guneykocyigit@gmail.com>
2019-01-15 15:03:43 +0100

    Add files via upload

---
+++ snippet.js
@@ -0,0 +1,28 @@
+/* Minified */
+(function (m, e, g, a, t, r, o, n) {
+    m['megatron']={};m['megatron'].name=t;
+    if(m[t] === undefined){m[t] = function(){(m[t].q = m[t].q || []).push(arguments);};m[t].d=r;}else{( console.log('N/A:' + t))};
+    o = e.createElement(g);
+    n = e.getElementsByTagName(g)[0];
+    o.async = true;
+    o.src = (a.indexOf(e.location.host) > -1) ? a : (e.location.host+a);
+    n.parentNode.insertBefore(o,n);
+})(window, document, 'script', 'https://URLTOLIBRARY.COM/megatron.js', 'mt', 'URLTOHITCOLLECTOR.COM/blabla');
+
+mt('set', 'enablePulse', true);
+mt('set', 'platformID', 'megatron-rocks123');
+mt('send', 'pageview');
+
+
+/* Explained */
+(function (win, doc, scrt, scriptSource, name, target, tag, justaparam) {
+    win['megatron']={};win['megatron'].name=name;
+    if(win[name] === undefined){win[name]= function(){(win[name].q = win[name].q || []).push(arguments);};win[name].s=target;}else{( console.log('N/A:' + name))};
+    tag = doc.createElement(scrt);
+    justaparam = doc.getElementsByTagName(scrt)[0];
+    tag.async = true;
+    tag.src = (scriptSource.indexOf(doc.location.host) > -1) ? scriptSource : (doc.location.host+scriptSource);
+    justaparam.parentNode.insertBefore(tag,free);
+    
+})(window, document, 'script', '/megatron.js', 'mgtrn','URLTOHITCOLLECTOR.COM/blabla');
+
